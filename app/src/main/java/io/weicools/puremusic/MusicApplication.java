e0c38bf14722e19e273556bb50cd256b6a36f3b0
Weicools <2496273763@qq.com>
2017-11-22 10:12:39 +0800

    add Stetho, LeakCanary

--- app/src/main/java/io/weicools/puremusic/MusicApplication.java
+++ app/src/main/java/io/weicools/puremusic/MusicApplication.java
@@ -2,6 +2,8 @@ package io.weicools.puremusic;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -23,6 +25,14 @@ public class MusicApplication extends Application {
         AppCache.init(this);
         ActivityObserver.init(this);
         initOkHttpUtils();
+
+        Stetho.initializeWithDefaults(this);
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
     private void initOkHttpUtils() {
