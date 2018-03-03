0e939289077b9c8547554b21e3dcac9e1ea800d3
lecymeng <1570682285@qq.com>
2018-03-04 02:29:58 +0800

    更新Adapter

--- app/src/main/java/io/weicools/puremusic/adapter/PlayListAdapter.java
+++ app/src/main/java/io/weicools/puremusic/adapter/PlayListAdapter.java
@@ -1,6 +1,6 @@
 package io.weicools.puremusic.adapter;
 
-import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,197 +8,97 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-
 import java.util.List;
 
 import io.weicools.puremusic.R;
-import io.weicools.puremusic.http.HttpCallback;
-import io.weicools.puremusic.http.HttpClient;
-import io.weicools.puremusic.model.OnlineMusic;
-import io.weicools.puremusic.model.OnlineMusicList;
-import io.weicools.puremusic.model.SongListInfo;
+import io.weicools.puremusic.model.Music;
+import io.weicools.puremusic.service.AudioPlayer;
+import io.weicools.puremusic.util.CoverLoader;
+import io.weicools.puremusic.util.FileUtil;
 
 /**
  * Author: weicools
  * Time: 2017/11/27 上午9:48
+ *
+ * 本地音乐列表适配器
  */
 
 public class PlayListAdapter extends BaseAdapter {
-    private static final int TYPE_PROFILE = 0;
-    private static final int TYPE_MUSIC_LIST = 1;
+    private List<Music> musicList;
+    private OnMoreClickListener listener;
+    private boolean isPlaylist;
 
-    private Context mContext;
-    private List<SongListInfo> mData;
+    public PlayListAdapter(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
-    public PlayListAdapter(List<SongListInfo> data) {
-        this.mData = data;
+    public void setIsPlaylist(boolean isPlaylist) {
+        this.isPlaylist = isPlaylist;
+    }
+
+    public void setOnMoreClickListener(OnMoreClickListener listener) {
+        this.listener = listener;
     }
 
     @Override
     public int getCount() {
-        return mData.size();
+        return musicList.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return mData.get(i);
+    public Object getItem(int position) {
+        return musicList.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
-        return i;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if (mData.get(position).getType().equals("#")) {
-            return TYPE_PROFILE;
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_music, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
         } else {
-            return TYPE_MUSIC_LIST;
+            holder = (ViewHolder) convertView.getTag();
         }
-    }
-
-    @Override
-    public boolean isEnabled(int position) {
-        return getItemViewType(position) == TYPE_MUSIC_LIST;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 2;
-    }
-
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        mContext = parent.getContext();
-        ViewHolderProfile holderProfile;
-        ViewHolderMusicList holderMusicList;
-
-        SongListInfo songListInfo = mData.get(position);
-        int itemViewType = getItemViewType(position);
-        switch (itemViewType) {
-            case TYPE_PROFILE:
-                if (view == null) {
-                    view = LayoutInflater.from(mContext).inflate(R.layout.item_play_list_profile, parent, false);
-                    holderProfile = new ViewHolderProfile(view);
-                    view.setTag(holderProfile);
-                } else {
-                    holderProfile = (ViewHolderProfile) view.getTag();
-                }
-                holderProfile.tvProfile.setText(songListInfo.getTitle());
-                break;
-            case TYPE_MUSIC_LIST:
-                if (view == null) {
-                    view = LayoutInflater.from(mContext).inflate(R.layout.item_play_music_list, parent, false);
-                    holderMusicList = new ViewHolderMusicList(view);
-                    view.setTag(holderMusicList);
-                } else {
-                    holderMusicList = (ViewHolderMusicList) view.getTag();
-                }
-                getMusicListInfo(songListInfo, holderMusicList);
-                holderMusicList.vDivider.setVisibility(isShowDivider(position) ? View.VISIBLE : View.GONE);
-                break;
-            default:
-                break;
-        }
-        return view;
+        holder.vPlaying.setVisibility((isPlaylist && position == AudioPlayer.getInstance().getPlayPosition()) ? View.VISIBLE : View.INVISIBLE);
+        Music music = musicList.get(position);
+        Bitmap cover = CoverLoader.getInstance().loadThumbnail(music);
+        holder.ivCover.setImageBitmap(cover);
+        holder.tvTitle.setText(music.getTitle());
+        String artist = FileUtil.getArtistAndAlbum(music.getArtist(), music.getAlbum());
+        holder.tvArtist.setText(artist);
+        holder.ivMore.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onMoreClick(position);
+            }
+        });
+        holder.vDivider.setVisibility(isShowDivider(position) ? View.VISIBLE : View.GONE);
+        return convertView;
     }
 
     private boolean isShowDivider(int position) {
-        return position != mData.size() - 1;
+        return position != musicList.size() - 1;
     }
 
-    private void getMusicListInfo(final SongListInfo songListInfo, final ViewHolderMusicList holderMusicList) {
-        if (songListInfo.getCoverUrl() == null) {
-            holderMusicList.tvMusic1.setTag(songListInfo.getTitle());
-            holderMusicList.ivCover.setImageResource(R.drawable.default_cover);
-            holderMusicList.tvMusic1.setText("1.加载中…");
-            holderMusicList.tvMusic2.setText("2.加载中…");
-            holderMusicList.tvMusic3.setText("3.加载中…");
-
-            HttpClient.getSongListInfo(songListInfo.getType(), 3, 0, new HttpCallback<OnlineMusicList>() {
-                @Override
-                public void onSuccess(OnlineMusicList onlineMusicList) {
-                    if (onlineMusicList == null || onlineMusicList.getSong_list() == null) {
-                        return;
-                    }
-                    if (!songListInfo.getTitle().equals(holderMusicList.tvMusic1.getTag())) {
-                        return;
-                    }
-                    parse(onlineMusicList, songListInfo);
-                    setData(songListInfo, holderMusicList);
-                }
-
-                @Override
-                public void onFail(Exception e) {
-
-                }
-            });
-        } else {
-            holderMusicList.tvMusic1.setTag(null);
-            setData(songListInfo, holderMusicList);
-        }
-    }
-
-    private void parse(OnlineMusicList onlineMusicList, SongListInfo songListInfo) {
-        List<OnlineMusic> onlineMusics = onlineMusicList.getSong_list();
-        songListInfo.setCoverUrl(onlineMusicList.getBillboard().getPic_s260());
-        if (onlineMusics.size() >= 1) {
-            songListInfo.setMusic1(mContext.getString(R.string.song_list_item_title_1,
-                    onlineMusics.get(0).getTitle(), onlineMusics.get(0).getArtist_name()));
-        } else {
-            songListInfo.setMusic1("");
-        }
-        if (onlineMusics.size() >= 2) {
-            songListInfo.setMusic2(mContext.getString(R.string.song_list_item_title_2,
-                    onlineMusics.get(1).getTitle(), onlineMusics.get(1).getArtist_name()));
-        } else {
-            songListInfo.setMusic2("");
-        }
-        if (onlineMusics.size() >= 3) {
-            songListInfo.setMusic3(mContext.getString(R.string.song_list_item_title_3,
-                    onlineMusics.get(2).getTitle(), onlineMusics.get(2).getArtist_name()));
-        } else {
-            songListInfo.setMusic3("");
-        }
-    }
-
-    private void setData(SongListInfo songListInfo, ViewHolderMusicList holderMusicList) {
-        holderMusicList.tvMusic1.setText(songListInfo.getMusic1());
-        holderMusicList.tvMusic2.setText(songListInfo.getMusic2());
-        holderMusicList.tvMusic3.setText(songListInfo.getMusic3());
-
-        // TODO: 2017/11/27 error and preload 
-        Glide.with(mContext)
-                .load(songListInfo.getCoverUrl())
-                .into(holderMusicList.ivCover);
-    }
-
-    private static class ViewHolderProfile {
-        private TextView tvProfile;
-
-        ViewHolderProfile(View view) {
-            super();
-
-            tvProfile = view.findViewById(R.id.tv_profile);
-        }
-    }
-
-    private static class ViewHolderMusicList {
+    private static class ViewHolder {
+        private View vPlaying;
         private ImageView ivCover;
-        private TextView tvMusic1;
-        private TextView tvMusic2;
-        private TextView tvMusic3;
+        private TextView tvTitle;
+        private TextView tvArtist;
+        private ImageView ivMore;
         private View vDivider;
 
-        ViewHolderMusicList(View view) {
-            super();
-
+        ViewHolder(View view) {
+            vPlaying = view.findViewById(R.id.view_playing);
             ivCover = view.findViewById(R.id.iv_cover);
-            tvMusic1 = view.findViewById(R.id.tv_music_1);
-            tvMusic2 = view.findViewById(R.id.tv_music_2);
-            tvMusic3 = view.findViewById(R.id.tv_music_3);
+            tvTitle = view.findViewById(R.id.tv_title);
+            tvArtist = view.findViewById(R.id.tv_artist);
+            ivMore = view.findViewById(R.id.iv_more);
             vDivider = view.findViewById(R.id.v_divider);
         }
     }
